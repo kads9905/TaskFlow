607f1667f1b8176d3dbc179a6832cb9d3c2b8a7e
kads9905 <kadambari9905@gmail.com>
2026-08-27 17:30:52 +0530

    feat: configure server entry point and database startup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// entry point for application
+
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
+
+// loads value from .env
+dotenv.config({
+    path: "./.env"
+});
+
+// connects atlas before starting
+connectDB()
+.then(() => {
+    // starts express only after db is ready
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+})
+// handles db error
+.catch((err) => {
+    console.log("MongoDB connection failed!", err);
+});
